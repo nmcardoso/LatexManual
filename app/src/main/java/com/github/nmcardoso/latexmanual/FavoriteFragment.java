ae54e4c6973d85c6fffcba74caf2ad16bc3ff790
Natanael <natanael_magalhaes@msn.com>
2016-10-16 11:01:40 -0200

    pages navigator and empty message added

--- app/src/main/java/com/github/nmcardoso/latexmanual/FavoriteFragment.java
+++ app/src/main/java/com/github/nmcardoso/latexmanual/FavoriteFragment.java
@@ -8,11 +8,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.List;
 
 
 public class FavoriteFragment extends Fragment {
+
+    private static final int RESULTS_PER_PAGE = 20;
+    private int currentPage = 1;
+    private int totalPages;
+    private DatabaseHelper dbHelper;
+    private FavoriteAdapter favoriteAdapter;
+    private TextView txtFavNav;
+    private List<Favorite> favoritesList;
+
 
     public FavoriteFragment() {
     }
@@ -23,13 +36,60 @@ public class FavoriteFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
 
-        DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
+        dbHelper = new DatabaseHelper(getActivity());
 
         RecyclerView rvFavorites = (RecyclerView) view.findViewById(R.id.rv_favorites);
-        List<Favorite> favoritesList = dbHelper.getFavorites(15);
-        FavoriteAdapter favoriteAdapter = new FavoriteAdapter(getActivity(), favoritesList);
+        favoritesList = dbHelper.getFavorites(RESULTS_PER_PAGE);
+        favoriteAdapter = new FavoriteAdapter(getActivity(), favoritesList);
         rvFavorites.setAdapter(favoriteAdapter);
         rvFavorites.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        LinearLayout favoritesNav = (LinearLayout) view.findViewById(R.id.favorites_nav);
+        txtFavNav = (TextView) view.findViewById(R.id.txt_fav_nav);
+        ImageButton ibNext = (ImageButton) view.findViewById(R.id.ib_fav_next);
+        ImageButton ibPrev = (ImageButton) view.findViewById(R.id.ib_fav_prev);
+
+        int favCount = dbHelper.getFavoritesCount();
+        totalPages = (int) Math.ceil((float) favCount / RESULTS_PER_PAGE);
+
+        if (favCount == 0) {
+            ScrollView svHistory = (ScrollView) view.findViewById(R.id.sv_favorites);
+            TextView txtEmpty = (TextView) view.findViewById(R.id.txt_empty);
+            svHistory.setVisibility(View.GONE);
+            txtEmpty.setVisibility(View.VISIBLE);
+        } else if (favCount > RESULTS_PER_PAGE) {
+            favoritesNav.setVisibility(View.VISIBLE);
+            txtFavNav.setText(String.format(getResources().getString(R.string.i_of_n),
+                    1, totalPages));
+
+            ibPrev.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (currentPage > 1) {
+                        currentPage--;
+                        favoritesList = dbHelper.getFavorites(RESULTS_PER_PAGE,
+                                (currentPage - 1) * RESULTS_PER_PAGE);
+                        favoriteAdapter.swap(favoritesList);
+                        txtFavNav.setText(String.format(getResources().getString(R.string.i_of_n),
+                                currentPage, totalPages));
+                    }
+                }
+            });
+
+            ibNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (currentPage < totalPages) {
+                        favoritesList = dbHelper.getFavorites(RESULTS_PER_PAGE,
+                                currentPage * RESULTS_PER_PAGE);
+                        favoriteAdapter.swap(favoritesList);
+                        currentPage++;
+                        txtFavNav.setText(String.format(getResources().getString(R.string.i_of_n),
+                                currentPage, totalPages));
+                    }
+                }
+            });
+        }
 
         return view;
     }
