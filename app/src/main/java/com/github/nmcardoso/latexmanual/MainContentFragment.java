f38c97fe0b99568bde913fda21f4e770cb6f6081
Natanael <natanael_magalhaes@msn.com>
2016-10-18 11:12:04 -0200

    field renamed, stats structure changed

--- app/src/main/java/com/github/nmcardoso/latexmanual/MainContentFragment.java
+++ app/src/main/java/com/github/nmcardoso/latexmanual/MainContentFragment.java
@@ -31,24 +31,24 @@ public class MainContentFragment extends Fragment {
         List<Card> cardsList = new ArrayList<>();
         cardsList.add(
                 new Card.CardBuilder(getString(R.string.most_viewed), dbHelper.getMostViewed(5))
-                .flag(CardListAdapter.MOST_VIEWED)
-                .headerBackground(R.color.orange1)
-                .headerIcon(R.drawable.ic_plus)
-                .build()
+                        .listItemType(CardListAdapter.MOST_VIEWED)
+                        .headerBackground(R.color.orange1)
+                        .headerIcon(R.drawable.ic_plus)
+                        .build()
         );
         cardsList.add(
                 new Card.CardBuilder(getString(R.string.history), dbHelper.getHistory(5))
-                .flag(CardListAdapter.HISTORY)
-                .headerBackground(R.color.purple1)
-                .headerIcon(R.drawable.ic_menu_history)
-                .build()
+                        .listItemType(CardListAdapter.HISTORY)
+                        .headerBackground(R.color.purple1)
+                        .headerIcon(R.drawable.ic_menu_history)
+                        .build()
         );
         cardsList.add(
                 new Card.CardBuilder(getString(R.string.favorites), dbHelper.getFavorites(5))
-                .flag(CardListAdapter.FAVORITE)
-                .headerBackground(R.color.blue1)
-                .headerIcon(R.drawable.ic_star)
-                .build()
+                        .listItemType(CardListAdapter.FAVORITE)
+                        .headerBackground(R.color.blue1)
+                        .headerIcon(R.drawable.ic_star)
+                        .build()
         );
         List<Pair<String, Integer>> stats = new ArrayList<>();
         stats.add(new Pair<String, Integer>("Doc Count", dbHelper.getDocumentationCount()));
@@ -56,15 +56,11 @@ public class MainContentFragment extends Fragment {
         stats.add(new Pair<String, Integer>("Unique Hist Count", dbHelper.getUniqueHistoryCount()));
         stats.add(new Pair<String, Integer>("Fav Count", dbHelper.getFavoritesCount()));
         cardsList.add(
-                new Card.CardBuilder(getString(R.string.statistics),
-                        String.format(getString(R.string.stats_content),
-                                dbHelper.getDocumentationCount(),
-                                dbHelper.getHistoryCount(),
-                                dbHelper.getUniqueHistoryCount(),
-                                dbHelper.getFavoritesCount()))
-                .headerBackground(R.color.cardview_dark_background)
-                .headerIcon(R.drawable.ic_trending_up)
-                .build()
+                new Card.CardBuilder(getString(R.string.statistics), stats)
+                        .listItemType(CardListAdapter.STATISTICS)
+                        .headerBackground(R.color.cardview_dark_background)
+                        .headerIcon(R.drawable.ic_trending_up)
+                        .build()
         );
 
         CardRecyclerAdapter adapter = new CardRecyclerAdapter(getActivity(), cardsList);
