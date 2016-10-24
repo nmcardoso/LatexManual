df7abc0d13c510bb8c83bd36adf11adb21a8247b
Natanael <natanael_magalhaes@msn.com>
2016-10-24 15:02:07 -0200

    implementation with Pattern and Matcher

--- app/src/main/java/com/github/nmcardoso/latexmanual/TextUtil.java
+++ app/src/main/java/com/github/nmcardoso/latexmanual/TextUtil.java
@@ -1,36 +1,49 @@
 package com.github.nmcardoso.latexmanual;
 
 import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.style.BackgroundColorSpan;
-import android.util.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextUtil {
-    public static Spannable highlight(String content, String token) {
-        List<Pair<Integer, Integer>> pairs = new ArrayList<>();
-        Spannable spanText = Spannable.Factory.getInstance().newSpannable(content);
-        int count = 0;
-        String contentLC = content.toLowerCase();
-        String tokenLC = token.toLowerCase();
+    public static Spannable highlight(String text, String token) {
+        Spannable spannable = new SpannableString(text);
+        String regex = token.replace(" ", "|");
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
 
-        if (!contentLC.contains(tokenLC) || token.equals("")) {
-            return spanText;
+        while (matcher.find()) {
+            spannable.setSpan(new BackgroundColorSpan(0xFFFFFF00), matcher.start(),
+                    matcher.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
 
-        while (count < contentLC.length() &&
-                contentLC.substring(count).contains(tokenLC)) {
-            int index = contentLC.indexOf(tokenLC);
-            pairs.add(new Pair<Integer, Integer>(index, index + tokenLC.length()));
-            count += index + tokenLC.length();
+        return spannable;
+    }
+
+    public static Spannable highlightNearText(String text, String token) {
+        String regex = token.replace(" ", "|");
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        Spannable spannable;
+
+        if (matcher.find()) {
+            text = text.substring(matcher.start(), text.length() > (matcher.start() + 100) ?
+                    (matcher.start() + 100) : text.length() - 1);
+
+            spannable = new SpannableString(text);
+            matcher.reset(text);
+
+            while (matcher.find()) {
+                spannable.setSpan(new BackgroundColorSpan(0xFFFFFF00), matcher.start(),
+                        matcher.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        } else {
+            spannable = new SpannableString(text);
         }
 
-        for (int i = 0; i < pairs.size(); i++) {
-            spanText.setSpan(new BackgroundColorSpan(0xFFFFFF00), pairs.get(i).first,
-                    pairs.get(i).second, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-
-        return spanText;
+        return spannable;
     }
 }
